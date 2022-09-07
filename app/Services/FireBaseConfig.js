5f012db060a330947b426732f9e163deab3b24bd
Chriss78Vera <chrissvera75@gmail.com>
2022-09-07 14:48:27 -0500

    Cambios en la vista de revision de horas

--- app/Services/FireBaseConfig.js
+++ app/Services/FireBaseConfig.js
@@ -9,10 +9,10 @@ export const loadFirebaseConfiguration = () => {
 };
 
 const firebaseConfig = {
-  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  projectId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  apiKey: "AIzaSyBzjefHHmkLhC-qvd-ikSodWWRx2QHQLKg",
+  authDomain: "miproyecto-d157b.firebaseapp.com",
+  projectId: "miproyecto-d157b",
+  storageBucket: "miproyecto-d157b.appspot.com",
+  messagingSenderId: "510856880730",
+  appId: "1:510856880730:web:3b84cf279f835573196e63"
 };
